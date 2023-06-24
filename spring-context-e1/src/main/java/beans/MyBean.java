c7a1a582f3ef80788d54ceebcb6e382e428cd9b5
KhaledM0hamed <khaledmzakria1@gmail.com>
2023-06-24 18:12:49 +0300

    L1-second way to declear bean

--- spring-context-e1/src/main/java/beans/MyBean.java
+++ spring-context-e1/src/main/java/beans/MyBean.java
@@ -1,8 +1,21 @@
-package org.example.beans;
+package beans;
 
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
 public class MyBean {
 
     private String text;
+
+    /**
+     * Called after the instance creation 
+     */
+    @PostConstruct
+    private void init() {
+        this.text = "hi";
+    }
 
     public void setText(String text) {
         this.text = text;
--- spring-context-e1/src/main/java/main/Main.java
+++ spring-context-e1/src/main/java/main/Main.java
@@ -1,19 +1,16 @@
-package org.example.main;
+package main;
 
-import org.example.beans.MyBean;
-import org.example.config.ProjectConfig;
+import beans.MyBean;
+import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        /**
-         * configurations can come from XML or
-         * Annotations
-         */
+
         try (var context = new AnnotationConfigApplicationContext(ProjectConfig.class)) {
 
-            MyBean b1 = context.getBean("b1", MyBean.class);
-            MyBean b2 = context.getBean("b2", MyBean.class);
+            MyBean b1 = context.getBean(MyBean.class);
+            MyBean b2 = context.getBean(MyBean.class);
 
             System.out.println(b1.getText());
             System.out.println(b2.getText());
